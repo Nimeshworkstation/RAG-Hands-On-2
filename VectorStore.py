9f93a92573620ead6b6b53c65fd47fa39607de65
Nimesh Ghimire <nimesh.ghimire99@gmail.com>
2026-05-03 09:05:39 +0200

    loader, chunker, embedderand vector store

---
+++ VectorStore.py
@@ -0,0 +1,67 @@
+import chromadb
+import os
+import uuid
+import hashlib
+from langchain_core.documents import Document
+from typing import List
+import numpy as np
+
+
+class VectorStore:
+    def __init__(
+        self,
+        collection_name: str = "pdf-documents",
+        persist_directory: str = "../data/vector_store",
+    ):
+        self.collection_name = collection_name
+        self.persist_directory = persist_directory
+        self.client = None
+        self.collection = None
+        self._initialize_store()
+
+    def _initialize_store(self):
+        try:
+            os.makedirs(self.persist_directory, exist_ok=True)
+            self.client = chromadb.PersistentClient(path=self.persist_directory)
+            self.collection = self.client.get_or_create_collection(
+                name=self.collection_name,
+                metadata={"description": "PDF document embedding for RAG"},
+            )
+            print(f"Vector Store initialized. Collection: {self.collection_name}")
+            print(f"Existing documents in collection: {self.collection.count()}")
+        except Exception as e:
+            print(f"Error occured {e}")
+            raise
+
+    def create_chunk_id(self, chunk):
+        source = chunk.metadata.get("source", "")
+        page = chunk.metadata.get("page", "")
+        text = chunk.page_content
+        raw = f"{source}|{page}|{text}".encode("utf-8")
+        return hashlib.sha256(raw).hexdigest()
+
+    def add_documents(self, documents: List[Document], embeddings: np.ndarray):
+        if len(documents) != len(embeddings):
+            raise RuntimeError("Something went wrong")
+        print(f"Adding {len(documents)} vector")
+
+        # prepare data for chroma db
+        ids = []
+        documents_text = []
+        embeddings_list = []
+        metadatas = []
+
+        for chunk, embedding in zip(documents, embeddings):
+            ids.append(self.create_chunk_id(chunk))
+            documents_text.append(chunk.page_content)
+            embeddings_list.append(embedding.tolist())
+            metadatas.append(chunk.metadata)
+
+        self.collection.upsert(
+            ids=ids,
+            documents=documents_text,
+            embeddings=embeddings_list,
+            metadatas=metadatas,
+        )
+
+        print(f"[INFO] Stored {len(documents)} chunks in ChromaDB")
---
+++ try.py
@@ -0,0 +1,104 @@
+from pathlib import Path
+from langchain_core.documents import Document
+from typing import List
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    TextLoader,
+    DirectoryLoader,
+)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+
+def embed_chunks(documents):
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+    )
+    for doc in documents:
+        doc_vectors = embeddings.embed_documents(doc.page_content)
+
+
+def load_documents_directory_loader(data_dir: Path):
+    documents = []
+    dir_loader = DirectoryLoader(
+        path=data_dir,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader,
+        show_progress=True,
+    )
+    documents.extend(dir_loader.load())
+    return documents
+
+
+def load_documents(data_dir: Path) -> List[Document]:
+    documents: List[Document] = []
+    len_total_files = len([item for item in data_dir.iterdir()])
+    print(f"found {len_total_files} files/objects to process")
+
+    for file_path in data_dir.iterdir():
+
+        if not file_path.is_file():
+            print(
+                f"{file_path} : is not a file  ! Processing Aborting ..",
+            )
+            continue
+        file_ext = ((str(file_path).split("."))[-1]).lower()
+        if file_ext not in ["txt", "pdf"]:
+            print(f"Wrong file format {file_path} ! Processing Aborting ..")
+            continue
+        print(f"processing {file_path.name} ")
+        try:
+            if file_ext == "pdf":
+                doctype = PyPDFLoader(file_path=file_path)
+                loader = doctype.load()
+            elif file_ext == "txt":
+                doctype = TextLoader(file_path=file_path, encoding="utf-8")
+                loader = doctype.load()
+            else:
+                continue
+        except Exception as e:
+            print("Error occured while loading document from !!", file_path, e)
+            continue
+        for docs in loader:
+            docs.metadata.update(
+                {
+                    "source": str(file_path),
+                    "filename": file_path.name,
+                    "extension": file_ext,
+                }
+            )
+        documents.extend(loader)
+        print(f" ✓ loaded {len(loader)} pages..")
+    print(f"Total {len(documents)} documents processed ✓")
+    return documents
+
+
+def split_chunk(
+    document: List[Document], chunk_size: int = 2000, chunk_overlap: int = 200
+):
+    total_chunks = []
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap, length_function=len
+    )
+
+    chunks = text_splitter.split_documents(documents=document)
+    total_chunks.extend(chunks)
+    print(f"Splitted {len(document)} Documents into {len(total_chunks)} chunks ✓ \n")
+    return total_chunks
+
+
+def main():
+    data_dir = Path("data")
+
+    if data_dir.exists():
+        data = load_documents(data_dir)
+
+    chunks = split_chunk(data)
+    for item in chunks:
+        print(item)
+        print("\n")
+    print(len(chunks))
+
+
+if __name__ == "__main__":
+    main()
